09508c195c5fc95c8f0d6425ef40112c7a8ad60b
Peter Savichev (proton) <psavichev@gmail.com>
2018-11-14 12:05:53 +0100

    fixed css

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
       {
         test: /\.css$/,  
         exclude: /node_modules/,  
-        use: ['css-loader'],
+        use: ['style-loader', 'css-loader'],
       }
     ]
   },
